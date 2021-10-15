027943db462c3a4a25a184b6a6b7d1a153aadaf9
Alvaro Nicoli <xmr.nkr@gmail.com>
2021-10-15 18:40:35 -0300

    feat: move to different port

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule as NativeConfigModule } from '@nestjs/config';
 import { validate } from './env.validation';
+import appConfig from './app.config';
 import dbConfig from './db.config';
 import jwtConfig from './jwt.config';
 
@@ -9,7 +10,7 @@ import jwtConfig from './jwt.config';
     NativeConfigModule.forRoot({
       validate,
       isGlobal: true,
-      load: [dbConfig, jwtConfig],
+      load: [appConfig, dbConfig, jwtConfig],
     }),
   ],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import appConfig from './config/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: false });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+
+  const config = app.get<ConfigType<typeof appConfig>>(appConfig.KEY);
+  await app.listen(config.port, () =>
+    console.log(`App running on port ${config.port}`),
+  );
 }
 bootstrap();
